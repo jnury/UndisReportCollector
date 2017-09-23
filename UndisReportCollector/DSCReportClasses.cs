f86ca300d173ad67bdf0c25463c87acfe5ef8f7b
Julien Nury <julien@nury.ch>
2017-09-23 22:20:11 +0200

    Initial Commit

---
+++ UndisReportCollector/DSCReportClasses.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UndisReportCollector
+{
+    public class DSCReport
+    {
+        public string JobId { get; set; }
+        public string OperationType { get; set; }
+        public string RefreshMode { get; set; }
+        public string Status { get; set; }
+        public string ReportFormatVersion { get; set; }
+        public string ConfigurationVersion { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool RebootRequested { get; set; }
+        public string[] Errors { get; set; }
+        public string[] StatusData { get; set; }
+        public Dictionary<string, string>[] AdditionalData { get; set; }
+    }
+
+    public class StatusData
+    {
+        public DateTime StartDate { get; set; }
+        public string[] IPV6Addresses { get; set; }
+        public int DurationInSeconds { get; set; }
+        public string CurrentChecksum { get; set; }
+        public string MetaData { get; set; }
+        public bool RebootRequested { get; set; }
+        public string Status { get; set; }
+        public string[] IPV4Addresses { get; set; }
+        public string LCMVersion { get; set; }
+        public int NumberOfResources { get; set; }
+        public string Type { get; set; }
+        public string Hostname { get; set; }
+        public string[] MACAddresses { get; set; }
+        public string Locale { get; set; }
+        public string Mode { get; set; }
+        public Resource[] ResourcesInDesiredState { get; set; }
+        public MetaConfiguration MetaConfiguration { get; set; }
+    }
+
+    public class Resource
+    {
+        public string SourceInfo { get; set; }
+        public string ModuleName { get; set; }
+        public string DurationInSeconds { get; set; }
+        public string InstanceName { get; set; }
+        public DateTime StartDate { get; set; }
+        public string ResourceName { get; set; }
+        public string ModuleVersion { get; set; }
+        public bool RebootRequested { get; set; }
+        public string ResourceId { get; set; }
+        public string ConfigurationName { get; set; }
+        public bool InDesiredState { get; set; }
+    }
+
+    public class MetaConfiguration
+    {
+        public string AgentId { get; set; }
+        public Manager[] ConfigurationDownloadManagers { get; set; }
+        public string ActionAfterReboot { get; set; }
+        public string[] LCMCompatibleVersions { get; set; }
+        public string LCMState { get; set; }
+        public Manager[] ResourceModuleManagers { get; set; }
+        public Manager[] ReportManagers { get; set; }
+        public int StatusRetentionTimeInDays { get; set; }
+        public string LCMVersion { get; set; }
+        public int MaximumDownloadSizeMB { get; set; }
+        public string ConfigurationMode { get; set; }
+        public int RefreshFrequencyMins { get; set; }
+        public bool RebootNodeIfNeeded { get; set; }
+        public string SignatureValidationPolicy { get; set; }
+        public string RefreshMode { get; set; }
+        public string[] DebugMode { get; set; }
+        public string LCMStateDetail { get; set; }
+        public bool AllowModuleOverwrite { get; set; }
+        public int ConfigurationModeFrequencyMins { get; set; }
+        public string[] SignatureValidations { get; set; }
+    }
+
+    public class Manager
+    {
+        public string RegistrationKey { get; set; }
+        public string ServerURL { get; set; }
+        public string ResourceId { get; set; }
+        public string[] ConfigurationNames { get; set; }
+    }
+}
---
+++ UndisReportCollector/Log.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace UndisReportCollector
+{
+
+    public class Log
+    {
+        private DateTime timestamp = DateTime.Now;
+        private string jobId = null;
+        private string operationType = null;
+        private string refreshMode = null;
+        private string status = null;
+        private bool rebootRequested = false;
+        private int durationInSeconds = 0;
+        private int numberOfResources = 0;
+        private string hostname = null;
+        private enum LevelEnum { UNDIFINED, CRITICAL, ERROR, WARN, INFO, VERBOSE };
+
+        /// <summary>
+        /// Folder where we write logs
+        /// </summary>
+        private string LogsFolder = ConfigurationManager.AppSettings["UndisLogsFolder"];   
+
+        public Log (DSCReport dscReport, StatusData statusData)
+        {
+            if (dscReport != null)
+            {
+                // If there is no status in DSC Report, Job is InProgress
+                if (dscReport.Status == null)
+                {
+                    this.status = "InProgress";
+                }
+                else
+                {
+                    this.status = dscReport.Status;
+                }
+
+                // Get data from DSC Report
+                this.jobId = dscReport.JobId;
+                this.operationType = dscReport.OperationType;
+                this.refreshMode = dscReport.RefreshMode;
+                this.rebootRequested = dscReport.RebootRequested;
+
+                // Get data from DSC StatusReport, if available
+                if (statusData != null)
+                {
+                    this.durationInSeconds = statusData.DurationInSeconds;
+                    this.numberOfResources = statusData.NumberOfResources;
+                    this.hostname = statusData.Hostname;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write log line in log file
+        /// </summary>
+        public void Flush ()
+        {
+            int configLogLevel = (int)LevelEnum.INFO;
+            int logLevel = (int)LevelEnum.INFO;
+
+            if (ConfigurationManager.AppSettings["UndisLogLevel"] != null)
+            {
+                if (LevelEnum.IsDefined(typeof(LevelEnum), ConfigurationManager.AppSettings["UndisLogLevel"].ToUpper()))
+                {
+                    configLogLevel = (int)LevelEnum.Parse(typeof(LevelEnum), ConfigurationManager.AppSettings["UndisLogLevel"].ToUpper());
+                }
+            }
+
+            switch (this.status)
+            {
+                case "InProgress":
+                    logLevel = (int)LevelEnum.VERBOSE;
+                    break;
+                case "Failed":
+                    logLevel = (int)LevelEnum.ERROR;
+                    break;
+            }
+
+            if (Directory.Exists(LogsFolder) && logLevel <= configLogLevel)
+            {
+                // Build log filename
+                var fileName = $"DSCReports_{timestamp.ToString("yyyyMMdd")}.log";
+                var fileFullPath = Path.Combine(LogsFolder, fileName);
+
+                // Build log line
+                string logLine = "";
+                logLine += timestamp.ToString("yyyy-MM-dd HH:mm:ss ");
+
+                logLine += Enum.GetName(typeof(LevelEnum), logLevel);
+
+                logLine += $" Hostname={this.hostname} OperationType={this.operationType} Status={this.status} RefreshMode={this.refreshMode}";
+                logLine += $" RebootRequested={this.rebootRequested} DurationInSeconds={this.durationInSeconds} NumberOfResources={this.numberOfResources} JobId={this.jobId}";
+
+                try
+                {
+                    // Write log line in log file
+                    using (StreamWriter file = new StreamWriter(fileFullPath, true))
+                    {
+                        file.WriteLine(logLine);
+                        file.Close();
+                    }
+                }
+                catch
+                {
+                    // TODO if needed
+                }
+            }
+        }
+    }
+}
+
